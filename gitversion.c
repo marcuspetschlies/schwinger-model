f1e55517ced4de0a29e339131e2ad85b7a222d5a
Marcus Petschlies <marcus.petschlies@web.de>
2021-06-25 06:56:04 +0200

    added giversion source file to version control

--- gitversion.c
+++ gitversion.c
@@ -1,1 +1,1 @@
-namespace cvc { const char *g_gitversion = "91574e29f73f841dfc6a396689337eb4be96c83f /home/marcuspe/notes/vl-lqcd-ss2021/exercises/Exercise7/c/build"; }
+namespace cvc { const char *g_gitversion = "8fac2bba79e6d68238a899451b3e38dc968956ef /home/marcuspe/notes/vl-lqcd-ss2021/codes/schwinger-model/build"; }
